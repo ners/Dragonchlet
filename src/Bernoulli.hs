df6b7fdc4f8124cb917542b9d2ca9b3b623cd37a
ners <ners@gmx.ch>
2021-05-31 12:45:49 +0200

    Use library for Bernoulli numbers

--- src/Bernoulli.hs
+++ src/Bernoulli.hs
@@ -1,10 +1,6 @@
-module Bernoulli ( bernoullis, faulhaber ) where
+module Bernoulli ( bernoullis ) where
 
-import Data.Bool (bool)
-import Data.Ratio (Rational, denominator, numerator, (%))
+import Math.NumberTheory.Recurrences.Bilinear ( bernoulli )
 
 bernoullis :: [Rational]
-bernoullis = head <$> faulhaber
-
-faulhaber :: [[Rational]]
-faulhaber = tail $ scanl (\rs n -> let xs = zipWith ((*) . (n %)) [2 ..] rs in 1 - sum xs : xs) [] [0 ..]
+bernoullis = bernoulli
